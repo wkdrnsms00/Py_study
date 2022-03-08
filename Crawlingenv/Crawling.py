c71155d86798943da62730ba48b78fb99abb6d69
wkdrnsms00 <hold5732@naver.com>
2022-03-08 16:56:58 +0900

    vscode 환경설정 후 네이버 만화 실시간 순위 크롤링

---
+++ Crawlingenv/Crawling.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+# response = urlopen('https://en.wikipedia.org/wiki/Main_Page')
+# soup = BeautifulSoup(response, 'html.parser')
+# for anchor in soup.find_all('a'):
+#     print(anchor.get('href', '/'))
+# 기본 예제
+
+headers = {"User-Agent": "Mozilla/5.0"}
+url =Request('https://comic.naver.com/index', headers=headers)
+response = urlopen(url)
+soup = BeautifulSoup(response, 'html.parser')
+f = open('Crawling1', 'w')
+rank = 1
+for anchor in soup.select('a.tit'):
+    data = str(rank)+"등 "+ anchor.get_text()
+    rank += 1
+    f.write(data)
+f.close()
