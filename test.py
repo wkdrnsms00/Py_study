50556154c892516a603624499fc0b696f9b9ae9c
전우석 <61926524+wkdrnsms00@users.noreply.github.com>
2022-07-19 11:23:50 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-# Python3 샘플 코드 #
-import requests
-
-url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
-params ={'serviceKey' : '서비스키', 'pageNo' : '1', 'numOfRows' : '10', 'startCreateDt' : '20200310', 'endCreateDt' : '20200315' }
-
-response = requests.get(url, params=params)
-print(response.content)
